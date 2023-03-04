2eccb27152118209cafbcc84bfa075ab32bfb3a7
Nouredine LAMAMRA <noreddine.lamamra@zsoft-consulting.com>
2023-03-04 14:46:54 +0100

    Add the jacoco testing configuration.

--- backend/src/main/java/com/planifcarbon/backend/BackendApplication.java
+++ backend/src/main/java/com/planifcarbon/backend/BackendApplication.java
@@ -1,11 +1,13 @@
 package com.planifcarbon.backend;
 
+import com.planifcarbon.backend.config.ExcludeFromJacocoGeneratedReport;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackendApplication {
 
+    @ExcludeFromJacocoGeneratedReport
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
