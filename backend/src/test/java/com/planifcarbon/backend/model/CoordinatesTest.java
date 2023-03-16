443c7655a7d0e72e56d933541924e8257a5e5d2b
raveneau <emilien.raveneau@gmail.com>
2023-03-16 15:14:17 +0100

    Refactor tests

--- backend/src/test/java/com/planifcarbon/backend/model/CoordinatesTest.java
+++ backend/src/test/java/com/planifcarbon/backend/model/CoordinatesTest.java
@@ -2,64 +2,76 @@ package com.planifcarbon.backend.model;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class CoordinatesTest extends Assertions {
 
-    // TODO move values out of tests.
-
     // Test for the constructor, the getters and the equals method
-    @Test
-    public void testCoordinates() {
-        Coordinates c = new Coordinates(1, 2);
-        assertEquals(1, c.getLatitude());
-        assertEquals(2, c.getLongitude());
+    @ParameterizedTest
+    @CsvSource({"1,2", "0,0", "-1,0", "0,180", "0,-180", "90,-180", "80.1,-80.4567"})
+    public void testCoordinates(double la, double lo) {
+        Coordinates c = new Coordinates(la, lo);
+        assertEquals(la, c.getLatitude());
+        assertEquals(lo, c.getLongitude());
     }
-    @Test
-    public void testCoordinatesWithNegativeValue() {
-        Coordinates c = new Coordinates(-1.34, 0);
-        assertEquals(-1.34, c.getLatitude());
-        assertEquals(0, c.getLongitude());
+
+    @ParameterizedTest
+    @CsvSource({"1,2", "0,0", "-1,0", "0,180", "0,-180", "90,-180", "80.1,-80.4567"})
+    public void testDoesNotThrow(double la, double lo) {
+
+        assertDoesNotThrow(() -> new Coordinates(la, lo));
     }
-    @Test
-    public void testCoordinatesWithCorrectLongitude() {
-        assertDoesNotThrow(() -> new Coordinates(90, 0));
-        assertDoesNotThrow(() -> new Coordinates(0, 0));
-        assertDoesNotThrow(() -> new Coordinates(-1, 0));
-    }
-    @Test
-    public void testCoordinatesWithCorrectLatitude() {
-        assertDoesNotThrow(() -> new Coordinates(0, 180));
-        assertDoesNotThrow(() -> new Coordinates(0, -180));
-        assertDoesNotThrow(() -> new Coordinates(0, 0));
-        assertDoesNotThrow(() -> new Coordinates(0, 179.999999999999));
-        assertDoesNotThrow(() -> new Coordinates(0, -179.999999999999));
-    }
-    @Test
-    public void testCoordinatesWithCorrectLatitudeAndLongitude() {
-        assertDoesNotThrow(() -> new Coordinates(90, -180));
-        assertDoesNotThrow(() -> new Coordinates(80.1, -80.4567));
-    }
-    @Test
-    public void testCoordinatesWithBadLongitude() {
-        assertThrows(IllegalArgumentException.class, () -> new Coordinates(90.1, 0));
-        assertThrows(IllegalArgumentException.class, () -> new Coordinates(-91, 0));
-    }
-    @Test
-    public void testCoordinatesWithBadLatitude() {
-        assertThrows(IllegalArgumentException.class, () -> new Coordinates(0, 180.00000000009));
-        assertThrows(IllegalArgumentException.class, () -> new Coordinates(0, -18142531.1584152));
+
+    @ParameterizedTest
+    @CsvSource({"90.1, 0", "-91, 0", "0, 180.00000000009", "0, -18142531.1584152"})
+    public void testThrows(double la, double lo) {
+
+        assertThrows(IllegalArgumentException.class, () -> new Coordinates(la, lo));
     }
 
 
     // Test for the toString method
-    @Test
-    public void testToStringIntCoordinates() {
-        Coordinates c = new Coordinates(1, 2);
-        assertEquals("1.0, 2.0", c.toString());
+    @ParameterizedTest
+    @CsvSource({"1,2,'1.0, 2.0'", "-1.2536, 2.456, '-1.2536, 2.456'"})
+    public void testToStringIntCoordinates(double la, double lo, String expected) {
+        Coordinates c = new Coordinates(la, lo);
+        assertEquals(expected, c.toString());
     }
+
+    // TODO equals tests
+
+    @ParameterizedTest
+    @CsvSource({"1,2,1,2", "0,0,0,0", "-1,0,-1,0", "0,180,0,180", "0,-180,0,-180", "90,-180,90,-180", "80.1,-80.4567,80.1,-80.4567"})
+    public void testEquals(double la1, double lo1, double la2, double lo2) {
+        Coordinates c1 = new Coordinates(la1, lo1);
+        Coordinates c2 = new Coordinates(la2, lo2);
+        assertEquals(c1, c2);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"0,0,0,0.00000001", "-1,0,-1,1", "0,180,0,181", "0,-180,0,-179", "90,-180,90,-179", "80.1,-80.4567,80.1,-80.4568"})
+    public void testNotEquals() {
+        Coordinates c1 = new Coordinates(1, 2);
+        Coordinates c2 = new Coordinates(1, 3);
+        assertNotEquals(c1, c2);
+    }
+
     @Test
-    public void testToStringDoubleCoordinates() {
-        Coordinates c = new Coordinates(-1.2536, 2.456);
-        assertEquals("-1.2536, 2.456", c.toString());
+    public void testNotEqualsNull() {
+        Coordinates c1 = new Coordinates(1, 2);
+        assertNotEquals(null, c1);
+    }
+
+    @Test
+    public void testNotEqualsOtherClass() {
+        Coordinates c1 = new Coordinates(1, 2);
+        assertNotEquals(new Object(), c1);
+    }
+
+    @Test
+    public void testNotEqualsString() {
+        Coordinates c1 = new Coordinates(1, 2);
+        assertNotEquals("1,2", c1);
     }
 }
