f83455b066f0643150dabb5813f12d08ec2855cb
noreddinelam <noreddinelamamra@gmail.com>
2023-03-18 17:26:22 +0100

    [23-implement-basic-front] Implement the first version of front part with basic tests.

--- frontend/src/app/getmesomewhere/getmesomewhere.component.spec.ts
+++ frontend/src/app/getmesomewhere/getmesomewhere.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormControl } from '@angular/forms';
 import { GetmesomewhereComponent } from './getmesomewhere.component';
 
 describe('GetmesomewhereComponent', () => {
@@ -21,5 +21,52 @@ describe('GetmesomewhereComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize the metroForm correctly', () => {
+    expect(component.metroForm).toBeDefined();
+    expect(component.metroForm.controls.from).toBeDefined();
+    expect(component.metroForm.controls.to).toBeDefined();
+    expect(component.metroForm.controls.timeToLeave).toBeDefined();
+    expect(component.metroForm.controls.options).toBeDefined();
+  });
+
+  it('should set "from" field as required', () => {
+    const control = component.metroForm.controls.from as FormControl;
+    control.setValue('');
+    expect(control.valid).toBeFalsy();
+    control.setValue('Bercy');
+    expect(control.valid).toBeTruthy();
+  });
+
+  it('should set "to" field as required', () => {
+    const control = component.metroForm.controls.to as FormControl;
+    control.setValue('');
+    expect(control.valid).toBeFalsy();
+    control.setValue('Gare de Lyon');
+    expect(control.valid).toBeTruthy();
+  });
+
+  it('should set "timeToLeave" field as required', () => {
+    const control = component.metroForm.controls.timeToLeave as FormControl;
+    control.setValue(null);
+    expect(control.valid).toBeFalsy();
+    control.setValue(new Date());
+    expect(control.valid).toBeTruthy();
+  });
+
+  it('should set "options" field as required', () => {
+    const control = component.metroForm.controls.options as FormControl;
+    control.setValue(null);
+    expect(control.valid).toBeFalsy();
+    control.setValue(['fastest']);
+    expect(control.valid).toBeTruthy();
+  });
+
+  describe('onSubmit', () => {
+    it('should do nothing', () => {
+      component.onSubmit();
+      expect(true).toBeTruthy();
+    });
   });
 });
