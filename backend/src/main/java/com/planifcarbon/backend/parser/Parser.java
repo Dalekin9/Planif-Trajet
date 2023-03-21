5460d8a5eddcb390d5bc846c1e5f45753b1159f6
Dalekin9 <99197171+Dalekin9@users.noreply.github.com>
2023-03-21 06:01:04 +0100

    getSchedule and getMetroStations implemented

--- backend/src/main/java/com/planifcarbon/backend/parser/Parser.java
+++ backend/src/main/java/com/planifcarbon/backend/parser/Parser.java
@@ -16,8 +16,7 @@ public class Parser{
         int minutes = Integer.parseInt(duration[0]);
         int seconds = Integer.parseInt(duration[1]);
 
-        seconds = seconds * 1000;           // Seconds to milliseconds
-        minutes = minutes * 60 * 1000;      // Minutes to milliseconds
+        minutes = minutes * 60 ;      // Minutes to seconds
 
         return minutes + seconds;
     }
@@ -27,8 +26,8 @@ public class Parser{
         int hours = Integer.parseInt(time[0]);
         int minutes = Integer.parseInt(time[1]);
 
-        hours = hours * 60 * 60 * 1000;
-        minutes = minutes *60 * 1000;
+        hours = hours * 60 * 60 ;
+        minutes = minutes * 60 ;
 
         return hours + minutes;
     }
@@ -71,7 +70,6 @@ public class Parser{
             currentNode.add( Float.parseFloat(coords[1]) );
             nodes.add(currentNode);
         }
-
         return nodes;
     }
 
@@ -98,37 +96,85 @@ public class Parser{
 
             segments.add(currentSegment);                        //Adds the segment to the collection
         }
-
         return segments;
     }
 
     public static Collection<Collection<Object>> getMetroStations(String filepath) throws FileNotFoundException {
-        //TODO
-        return null;
-    }
-
-    public static Collection<Collection<Object>> getSchedule(String filepath) throws FileNotFoundException {
         InputStream ins = new FileInputStream(filepath);
         Scanner scan = new Scanner(ins);
         String[] line;
 
-        Collection<Collection<Object>> timeTable = new ArrayList<>();
-        Collection<Object> currentTime;
+        Collection<Collection<Object>> segments = new ArrayList<>();
+        ArrayList<Object> currentSegment;
+        String[] coords;
+
+        while(scan.hasNextLine()) {
+
+            line = splitString(";", scan.nextLine());
+            //Each line contains 7 elements : name1, coords1, name2, coords2, line, time, dist
+
+            currentSegment = new ArrayList<>();
+
+            currentSegment.add(line[0]);                        //Name of the first station
+            coords = splitString(",", line[1]);             //Splits the coordinates to latitude and longitude
+            currentSegment.add( Float.parseFloat(coords[0]) );
+            currentSegment.add( Float.parseFloat(coords[1]) );
+
+            currentSegment.add(line[2]);                        //Name of the second station
+            coords = splitString(",", line[3]);
+            currentSegment.add( Float.parseFloat(coords[0]) );
+            currentSegment.add( Float.parseFloat(coords[1]) );
+
+            currentSegment.add(line[4]);                        //Name of the train line
+
+            currentSegment.add(durationStringToInt(line[5]));   //Travel time between stations
+            currentSegment.add(distanceStringToInt(line[6]));   //Distance between stations
+
+            segments.add(currentSegment);
+
+        }
+        return segments;
+    }
+
+    public static Map<String,Map<String,Collection<Integer>>> getSchedule(String filepath) throws FileNotFoundException {
+        InputStream ins = new FileInputStream(filepath);
+        Scanner scan = new Scanner(ins);
+        String[] line;
+
+        Map<String,Map<String, Collection<Integer>>> timeTable = new HashMap<>();
+        Map<String, Collection<Integer>> lineMap;
+        Collection<Integer> timesOfStation;
+
+        String currentLine;
+        String currentStation;
+        int currentTime;
 
         while(scan.hasNextLine()) {
 
             line = splitString(";", scan.nextLine());
             //Each line contains 3 elements : line, terminusStation, time
 
-            currentTime = new ArrayList<>();
+            currentLine = line[0];
+            currentStation = line[1];
+            currentTime = timeStringToInt(line[2]);
 
-            currentTime.add(line[0]);
-            currentTime.add(line[1]);
-            currentTime.add(timeStringToInt(line[2]));
+            lineMap = timeTable.get(currentLine);
 
-            timeTable.add(currentTime);
+            if(lineMap != null) {
+                timesOfStation = lineMap.get(currentStation);
+            }else{
+                timesOfStation = null;
+            }
+
+            if (lineMap == null){
+                timeTable.put(currentLine, new HashMap<>());
+                timeTable.get(currentLine).put(currentStation, new ArrayList<>(currentTime));
+            }else if(timesOfStation == null){
+                lineMap.put(currentStation, new ArrayList<>(currentTime));
+            }else{
+                timesOfStation.add(currentTime);
+            }
         }
-
         return timeTable;
     }
 }
